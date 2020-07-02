a4f5c411b353183636f5029c698cf98d3ce2e1ce
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-07-02 16:40:47 +0530

    Calculation functionality added.

--- calculator/script.js
+++ calculator/script.js
@@ -4,6 +4,8 @@ const multiply = (a,b) => a*b;
 const divide = (a,b) => a/b;
 
 const operate = (a,op,b) => {
+    a = Number(a);
+    b = Number(b);
     if(op === '+'){
         return add(a,b)
     }
@@ -21,14 +23,37 @@ const operate = (a,op,b) => {
 const number_buttons = document.querySelectorAll('.calculator-keys .number');
 const operater_button = document.querySelectorAll('.calculator-keys .operator');
 const calc_display = document.getElementById('display-screen');
+const equal_key = document.getElementById('equal-sign')
 
 
 let displayvalue = '';
+let operator = '';
+let prevvalue = '';
 
-const filldisplay = () => {
+const filldisplay = function() {
+    if(displayvalue.includes('.') && this.value === '.') return;
+
+    if (displayvalue.length > 10) return
+    
+    displayvalue += this.value;
     calc_display.value = displayvalue;    
 }
 
-number_buttons.forEach((number)=>{
+number_buttons.forEach(function(number){
     number.addEventListener('click',filldisplay);
+})
+
+const calculation = function(){
+    if(operator) {
+        displayvalue = operate(prevvalue, operator, displayvalue);
+        calc_display.value = displayvalue;
+    }
+
+    operator = this.value;
+    prevvalue = displayvalue;
+    displayvalue = ''
+}
+
+operater_button.forEach(function(operator){
+    operator.addEventListener('click',calculation)
 })
