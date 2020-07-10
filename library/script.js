a94c990350db709b70ffa6791a235fd3cfd4398a
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-07-10 20:09:35 +0530

    Stored objects in array

--- library/script.js
+++ library/script.js
@@ -1,7 +1,6 @@
 let addBook = document.getElementById('New-book')
 let submit = document.getElementById('submit')
 
-
 //array to store book objects
 let myLibrary = []
 
@@ -16,12 +15,38 @@ function Book(title, author, pages, isRead){
     }
 }
 
+//Popping up of form fixed
 function openForm(){
-    document.getElementById("popup-form").style.display = 'block';
+    if (addBook.value == "add"){
+        addBook.innerHTML = "Done"
+        document.getElementById("popup-form").style.display = 'block';
+        addBook.value = "done";
+        return
+    }
+    if (addBook.value == "done"){
+        addBook.innerHTML = "ADD NEW BOOK"
+        document.getElementById("popup-form").style.display = 'none';
+        addBook.value = 'add';
+        return
+    }
 }
 
-function closeForm(){
-    document.getElementById("popup-form").style.display = 'none';
-}
 addBook.addEventListener('click',openForm)
-submit.addEventListener('click',closeForm)
+
+function storeValues(e){
+    e.preventDefault()
+    let title = document.getElementById('bookname').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let status = document.getElementById('status');
+
+    document.getElementById('contact').reset()
+    if(status.checked){
+        myLibrary.push(new Book(title,author,pages,true))
+    }
+    else{
+        myLibrary.push(new Book(title,author,pages,false))
+    }
+}
+
+submit.addEventListener('click',storeValues)
