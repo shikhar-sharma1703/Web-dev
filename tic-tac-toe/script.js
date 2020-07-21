797cc52771bc5fd0a56d89dfb05fa17f4e73cbf1
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-07-21 21:17:43 +0530

    Basic functionality

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -7,8 +7,47 @@ const gameState = (function() {
         console.log(gameBoard)
     }  
 
+    const checkWinner = function(gamePlaying){
+        if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
+            gamePlaying = false;
+        }
+        else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
+            gamePlaying = false;
+        } 
+        else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
+            gamePlaying = false;
+        } 
+        else if (!gameBoard.includes('')) {
+            gamePlaying = false;
+        } 
+        else {
+            gamePlaying = true;
+        }
+        return gamePlaying;
+    }
+
+    const resetBoard = function() {
+        gameBoard = ['', '', '', '', '', '', '', '','']
+    }
+
+
     return {
-        gameBoard, storeMarker
+        gameBoard, storeMarker, checkWinner , resetBoard
     }
 })();
 
@@ -29,7 +68,7 @@ const displayController = (function(){
     //Change marker on turns
     const changePlayer = function(){
         if(currentPlayer == "X"){
-            currentPlayer = "0"
+            currentPlayer = "O"
         }
         else{
             currentPlayer = "X"
@@ -46,31 +85,39 @@ const displayController = (function(){
         domElements,
         getMarker,
         changePlayer,
-        fillBoard
+        fillBoard,
     }
 })()
 
 // Main function
 const playGame = (function(gameState,displayControl){
     let dom = displayControl.domElements;
+    let gamePlaying = true;
 
     const addMarkers = function(e){
-        if(e.target.matches('.cell')){
-            const cell = e.target;
-            const board = gameState.gameBoard;
-            const id = parseInt(e.target.id)
+        if(gamePlaying){
+            if(e.target.matches('.cell')){
+                const cell = e.target;
+                const board = gameState.gameBoard;
+                const id = parseInt(e.target.id)
 
-            console.log(id)
-            //Get marker
-            const marker = displayControl.changePlayer()
+                //Get marker
+                const marker = displayControl.changePlayer()
 
-            console.log(marker)
+                //STORE THE ELEMENT INSIDE THE GAMEBOARD ARRAY
+                gameState.storeMarker(id, marker)
 
-            //STORE THE ELEMENT INSIDE THE GAMEBOARD ARRAY
-            gameState.storeMarker(id, marker)
+                //Fill the board
+                displayControl.fillBoard(cell,board,id)
 
-            //Fill the board
-            displayControl.fillBoard(cell,board,id)
+                //Check for a winner
+                gamePlaying = gameState.checkWinner(gamePlaying);
+
+                if(!gamePlaying){
+                    gameState.resetBoard();
+                    alert("Game Over")
+                }
+            }
         }
     }
     
