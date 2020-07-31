fded60b374668ff34f2d1007cf41e4855cfc33b9
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-07-31 17:40:54 +0530

    Import/export

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
+import myName from './myName';
+
 
 function component(){
     const element = document.createElement('div');
     //Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], " ");
-
+    element.innerHTML = myName('Cody');
     return element;
 }
 
